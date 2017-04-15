1c92775f29b9c9ebe2daa87f26072082791ea2b6
SeWayan <se.wayan@googlemail.com>
2017-04-15 19:44:19 +0200

    Additional Comments & Tests
    Explanation -> \t, """
    Tested -> %r vs %s

--- ex10.py
+++ ex10.py
@@ -4,16 +4,24 @@ tabby_cat = "\tI'am tabbed in."
 persian_cat = "I'm split\non a line."
     # displays-> I'm \ a \ cat
 backslash_cat = "I'm \\ a \\ cat."
+    # 'Boooyah' displayed in next line below
+realest = "100 %\nBoooyah"
 
-    # creates a tab, *-> displayed in string
+    # creates a tab, '*' -> displayed in string
+    # ''' instead of """ -> preferences
 fat_cat = """
 I'll do a list
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+ """
+ # %r vs %s
+ # %r -> I luuuv them naptime, keepin it '100 %'
+ # %s -> I luuuv them naptime, keepin it 100 %
+fat_cat_fatso = "I luuuv them naptime, keepin it %s" % realest
 
 print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+print fat_cat_fatso
