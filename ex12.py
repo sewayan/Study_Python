b96f9e56aacbe6224399cb5a56c4b528eb552c43
SeWayan <se.wayan@googlemail.com>
2017-04-16 15:12:14 +0200

    Finished exercise 12

---
+++ ex12.py
@@ -0,0 +1,14 @@
+
+name = raw_input ("Name?")
+
+age = raw_input ("How old are you?")
+
+height = raw_input ("How tall are you?")
+
+weight = raw_input ("How much do you weigh?")
+
+    #Output %r -> replicated from raw_input
+print "Soooo, you're %r years old, %r tall and %r heavy." % (
+age, height, weight)
+
+#pydoc = Standard documentation module (Def of commands)
