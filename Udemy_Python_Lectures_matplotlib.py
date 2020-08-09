3679a8426b3fad26c0d3a0b6866fa3bdf26b272a
SeWayan <se.wayan@googlemail.com>
2020-08-09 18:30:39 +0200

    matplotlib

---
+++ Udemy_Python_Lectures_matplotlib.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  9 18:02:37 2020
+
+@author: sebastianwayan
+"""
+import matplotlib.pyplot as plt
+
+xs = [1, 2, 3] #Werte auf der x-Achse
+ys = [4, 7, 4] #Werte auf der y-Achse
+
+plt.plot(xs, ys)
